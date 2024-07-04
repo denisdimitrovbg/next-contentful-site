6fb56762cc4cfe3a11889e3dcc69805c94610dfe
Denis Dimitrov <denis.dimitrov@amusnet.com>
2024-07-04 13:55:47 +0300

    Adding types

---
+++ types.ts
@@ -0,0 +1,15 @@
+export type HeroQuery = {
+        heroCollection:  {
+        items: {
+        title: string
+        preTitle: string
+        subtitle: string
+        callToActionsCollection: {
+            items: {
+            label: string
+            link: string
+                }
+            }
+        }[]
+    }
+}
